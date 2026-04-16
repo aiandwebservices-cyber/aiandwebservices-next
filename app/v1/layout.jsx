fe515ebc48b6bc55ad7a34bf07e1391463719b2a
David Pulis <AIandWEBservices@gmail.com>
2026-04-16 13:10:06 -0400

    Fix: simplify V1 layout

--- app/v1/layout.jsx
+++ app/v1/layout.jsx
@@ -1,14 +1,3 @@
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-
 export default function V1Layout({ children }) {
-  return (
-    <>
-      <Header />
-      <main style={{ minHeight: '60vh' }}>
-        {children}
-      </main>
-      <Footer />
-    </>
-  );
+  return children;
 }
