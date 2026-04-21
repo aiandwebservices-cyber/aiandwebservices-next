250241e80ec352e94e2cdc1daa6a8e3a3fc27695
David Pulis <AIandWEBservices@gmail.com>
2026-04-21 15:17:51 -0400

    Suppress hydration warnings from third-party DOM injection (VPN/router __gcr attributes)

--- app/layout.jsx
+++ app/layout.jsx
@@ -63,7 +63,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${inter.variable} ${plusJakarta.variable}`}>
+    <html lang="en" className={`${inter.variable} ${plusJakarta.variable}`} suppressHydrationWarning={true}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
